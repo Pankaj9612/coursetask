a3d5bd0bad8db5e80636b776c29cd6cf65031f1e
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-03-24 16:51:51 +0530

    Add files via upload

---
+++ Var2.java
@@ -0,0 +1,16 @@
+class Var2
+{
+	public static void main(String args[])
+	{
+		// Declaration and initialization at one place..
+		byte score=106;
+		char grade= 'A';	
+		String name="Pankaj Gaikwad";
+		int age=22;
+
+		System.out.println(name);
+		System.out.println(grade);
+		System.out.println(score);
+		System.out.println(age);
+	}
+}
---
+++ var1.java
@@ -0,0 +1,22 @@
+class Var1
+{
+	public static void main(String args[])
+	{	
+		//Deaclaration of variable
+		byte score;
+		char grade;
+		String name;
+		int age;
+
+		//Initialzation of variable
+		score=106;
+		grade= 'A';
+		name="Pankaj Gaikwad";
+		age=22;
+
+		System.out.println(name);
+		System.out.println(grade);
+		System.out.println(score);
+		System.out.println(age);
+	}
+}
