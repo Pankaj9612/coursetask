6ae9c41bdd57072c96308f177c94b539df8e195d
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-03-28 19:19:52 +0530

    Add files via upload

---
+++ Compound1.java
@@ -0,0 +1,10 @@
+class Compound1
+{
+	public static void main(String args[])
+	{
+		int sal=45000;
+		//sal = sal + 15000;//salary increment by 15000
+		sal +=15000;
+		System.out.println(sal);
+	}
+}
---
+++ Compound2.java
@@ -0,0 +1,9 @@
+class Compound2
+{
+	public static void main(String args[])
+	{
+		int sal=45000;
+		sal -=100;
+		System.out.println(sal);//salary reduced by 100
+	}
+}
