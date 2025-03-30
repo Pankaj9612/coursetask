d7a059fd33c5f62b44545c320f0ebcf4bff0ec9e
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-03-30 12:31:27 +0530

    Add files via upload

---
+++ Logical2.java
@@ -0,0 +1,16 @@
+class Logical2
+{
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=20;
+		int c=30;
+		int d=0;
+
+		if(a<c || b>c)
+		{
+			 d=a+b+c;
+		}
+		System.out.println(d);//60
+	}
+}
