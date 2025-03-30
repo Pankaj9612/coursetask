6a8746ca03185c631832bc132f80d7102739c754
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-03-30 12:32:30 +0530

    Add files via upload

---
+++ Logical3.java
@@ -0,0 +1,12 @@
+class Logical3
+{
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=20;
+		if(a!=b)
+		{
+			System.out.println("a value is different from b.");//execute
+		}
+	}
+}
