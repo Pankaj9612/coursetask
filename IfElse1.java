4e8186eff842f5136d568bfca3e6d481dfcb4a44
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-02 16:54:50 +0530

    Add files via upload

---
+++ IfElse1.java
@@ -0,0 +1,15 @@
+class IfElse1
+{
+	public static void main(String args[])
+	{	
+		int num=-45;
+		if(num>0)
+		{
+			System.out.println("The given number is positive..");
+		}
+		else
+		{
+			System.out.println("The given number is Negative..");
+		}
+	}
+}
