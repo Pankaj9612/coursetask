ad8426b39cb1baceb085138376d1900ffaf64d57
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-02 16:53:29 +0530

    Add files via upload

---
+++ If2.java
@@ -0,0 +1,13 @@
+class If2
+{
+	public static void main(String args[])
+	{
+		System.out.println("Are you a paid student");
+		Boolean a=true;
+		if(a)
+		{
+			System.out.println("paid fees");//execute
+		}
+		System.out.println("Attend class..");
+	}
+}
