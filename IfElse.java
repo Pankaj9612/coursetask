fcf8cb15da8306dfda28cee72a8ff78d81f0e1f6
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-02 16:54:13 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,15 @@
+class IfElse
+{
+	public static void main(String args[])
+	{	
+		int num=45;
+		if(num%2==0)
+		{
+			System.out.println("The given number is even..");
+		}
+		else
+		{
+			System.out.println("The given number is odd..");
+		}
+	}
+}
