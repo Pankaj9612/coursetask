bc017cef6b900f00860b1dff647006f6a73870a4
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-04 15:06:51 +0530

    Add files via upload

---
+++ While2.java
@@ -0,0 +1,14 @@
+class While2
+{
+	public static void main(String args[])
+	{
+		int num=12;
+		int start=1;
+
+		while(start<=10)
+		{
+			System.out.println(num+" * "+start+"="+(num*start));
+			start++;
+		}
+	}
+}
