4849db45afb01504dbc985bf19ace3fa349ed312
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-05 14:59:49 +0530

    Add files via upload

---
+++ M3.java
@@ -0,0 +1,14 @@
+class M3
+{
+	public static int mul(int x,int y)
+	{
+		int z=x*y;
+		return z;
+	}
+	public static void main(String args[])
+	{
+		int a=mul(4,3);
+		System.out.println(a);
+
+	}
+}
