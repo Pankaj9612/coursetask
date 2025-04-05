0f25061182087de473e0ca4577cd29424b8d9a88
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-05 14:59:30 +0530

    Add files via upload

---
+++ M2.java
@@ -0,0 +1,16 @@
+class M2
+{
+	public static int sub()
+	{
+		int a=38;
+		int b=34;
+		int c=a-b;
+		return c;
+	}
+	public static void main(String args[])
+	{
+		M2 obj= new M2();
+		int d=obj.sub();
+		System.out.println(d);
+	}
+}
