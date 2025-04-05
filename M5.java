8f3758a1f7032e4577cc576729171a64304f4813
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-05 15:17:35 +0530

    Add files via upload

---
+++ M5.java
@@ -0,0 +1,21 @@
+class M5
+{
+	static void myName()
+	{
+		System.out.println("Pankaj Gaikwad");
+	}
+
+	public static void main(String args[])
+	{
+		/*myName();
+		myName();
+		myName();
+		myName();
+		myName();
+		myName();*/
+		for(int i=1;i<=10;i++)
+		{
+			myName();
+		}
+	}
+}
