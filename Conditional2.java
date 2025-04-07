48f3f5cfbfa500e8772008f2bf4c0ddfe1eb3c28
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-07 21:27:53 +0530

    Add files via upload

---
+++ Conditional2.java
@@ -0,0 +1,12 @@
+class Conditional2
+{
+	public static void main(String args[])
+	{
+		int num1=20;
+		int num2=34;
+		int num3=122;
+
+		int ans= num1<num2?(num1<num3?num1:num3):(num2<num3?num2:num3);
+		System.out.println(ans);
+	}
+}
