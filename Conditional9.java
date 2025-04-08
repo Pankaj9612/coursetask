9477b58f022c7a9805a8d39cfb32f5737ad2cd3a
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-08 16:14:10 +0530

    Add files via upload

---
+++ Conditional9.java
@@ -0,0 +1,9 @@
+class Conditional9
+{
+	public static void main(String[] args) 
+	{
+		char ch='A';
+		String ans=(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')?(ch+" it is a Owel alphabet"):(ch+" it is a Consonant alphabet");
+		System.out.println(ans);	
+	}
+}
