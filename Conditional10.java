02b58b0d12d43557451f619aa9e78ac231912562
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-08 16:14:32 +0530

    Add files via upload

---
+++ Conditional10.java
@@ -0,0 +1,9 @@
+class Conditional10
+{
+	public static void main(String[] args) 
+	{
+		char ch='a';
+		String ans=(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')?(ch+" it is a Owel alphabet"):(ch+" it is a Consonant alphabet");
+		System.out.println(ans);	
+	}
+}
