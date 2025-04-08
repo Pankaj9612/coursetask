c577d93ac3331843c18d1e36ff9c67ab66090d5b
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-08 16:13:27 +0530

    Add files via upload

---
+++ Conditional7.java
@@ -0,0 +1,9 @@
+class Conditional7
+{
+	public static void main(String[] args) 
+	{
+		char ch=56;
+		String ans=(ch>=48 && ch<=57)?(ch+" it is a Digit"):(ch+" it is a Not a Digit");
+		System.out.println(ans);	
+	}
+}
