f8b4bdd130297f37e7d0190d4b1ee976fb4f513a
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-09 17:57:18 +0530

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,29 @@
+class Length
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("MAX :"+Integer.MAX_VALUE);
+		System.out.println("MIN :"+Integer.MIN_VALUE);
+
+		System.out.println("MAX :"+Byte.MAX_VALUE);
+		System.out.println("MIN :"+Byte.MIN_VALUE);
+
+		System.out.println("MAX :"+Short.MAX_VALUE);
+		System.out.println("MIN :"+Short.MIN_VALUE);
+
+		System.out.println("MAX :"+Long.MAX_VALUE);
+		System.out.println("MIN :"+Long.MIN_VALUE);
+
+		System.out.println("MAX :"+(Character.MAX_VALUE+0));
+		System.out.println("MIN :"+(Character.MIN_VALUE+0));
+
+		System.out.println("MAX :"+Float.MAX_VALUE);
+		System.out.println("MIN :"+Float.MIN_VALUE);
+
+		System.out.println("MAX :"+Double.MAX_VALUE);
+		System.out.println("MIN :"+Double.MIN_VALUE);
+
+		//System.out.println("MIN :"+Boolean.MIN_VALUE);	
+		//System.out.println("MAX :"+Boolean.MAX_VALUE);
+	}
+}
