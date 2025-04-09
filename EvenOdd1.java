6e03bd267a948deb296ca68553f08cd92f9fa8bd
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-09 17:57:00 +0530

    Add files via upload

---
+++ EvenOdd1.java
@@ -0,0 +1,12 @@
+class EvenOdd1
+{
+	public static void main(String[] args) 
+	{
+		int num=32;
+		String ans=(num/2==num/2.0)?"Even":"Odd";
+		System.out.println(ans);	
+		int num1=59;
+		String ans1=((num1/2)*2==num1)?"Even":"Odd";
+		System.out.print(ans1);	
+	}
+}
