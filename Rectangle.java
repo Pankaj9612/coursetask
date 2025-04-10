91b454117831bee149bf2ebaa352b70e85a32e59
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-10 22:54:41 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+class Rectangle
+{
+	public static void main(String[] args) 
+	{
+		float width=4.5f;
+		final float height = 7.9f;
+		float area=height*width;
+		System.out.println("Area of Rectangle is: "+area);
+		float perimeter=(height+width)*2;
+		System.out.println("Perimeter of Circle is: "+perimeter);	
+	}
+}
