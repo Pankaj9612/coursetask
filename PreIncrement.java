5b3b4b23c6ea28e2d0b59d7cebcc7e5705e5b946
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-11 16:38:11 +0530

    Add files via upload

---
+++ PreIncrement.java
@@ -0,0 +1,14 @@
+class PreIncrement
+{
+	public static void main(String[] args) 
+	{
+		int coin=2;
+		System.out.println(coin++);
+		System.out.println(coin++);	
+		System.out.println(coin);	
+		System.out.println(coin++);	
+		System.out.println(coin++);	
+		System.out.println(coin);
+		System.out.println(coin++);	
+	}
+}
