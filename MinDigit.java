16e88e5c01d49620ccf4d3478ea821e11a6c3201
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-26 18:14:02 +0530

    Add files via upload

---
+++ MinDigit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class MinDigit
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int num=sc.nextInt();
+		int min=9;
+		while(num>0)
+		{
+			int rem=num%10;
+			if(rem<min)
+			{
+				min=rem;
+			}
+			num/=10;
+		}	
+		System.out.println(min);
+	}
+}
