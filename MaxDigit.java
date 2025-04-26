21e5b6920a4ef5f425bd32fdb75ae3ccee915102
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-04-26 18:13:47 +0530

    Add files via upload

---
+++ MaxDigit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class MaxDigit
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int num=sc.nextInt();
+		int max=0;
+		while(num>0)
+		{
+			int rem=num%10;
+			if(rem>max)
+			{
+				max=rem;
+			}
+			num/=10;
+		}	
+		System.out.println(max);
+	}
+}
