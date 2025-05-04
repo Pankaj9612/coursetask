1072a3a947db18dc264eae11197776a10945aa52
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-05-04 20:39:46 +0530

    Add files via upload

--- Lcm.java
+++ Lcm.java
@@ -1,23 +1,24 @@
-
-class Lcm {
-    
-    public static void main(String[] args)
-    {
-        
-        int a = 15, b = 25;
-
-        
-        int ans = (a > b) ? a : b;
-
-        
-        while (true) {
-            if (ans % a == 0 && ans % b == 0)
-                break;
-            ans++;
-        }
-
-        
-        System.out.println("LCM of " + a + " and " + b
-                           + " : " + ans);
-    }
+import java.util.Scanner;
+class Lcm
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a first number: ");
+		int n1=sc.nextInt();
+		System.out.println("Enter a second number: ");
+		int n2=sc.nextInt();
+		
+		int lcm=(n1>n2)?n1:n2;
+		int i=1;
+		while(true)
+		{
+			if((lcm*i)%n1==0 && (lcm*i)%n2==0)
+			{
+				System.out.println("the lcm of number is: "+(lcm*i));
+				break;
+			}
+			i++;
+		}
+	}
 }
