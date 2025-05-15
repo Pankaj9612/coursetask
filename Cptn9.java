0b81824161eb787b9a571cb2a9f4711705dd5462
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-05-15 16:52:13 +0530

    Add files via upload

---
+++ Cptn9.java
@@ -0,0 +1,18 @@
+class Cptn9
+{
+	public static void main(String[] args) 
+	{
+		int n=11;
+		for (int i=0;i<n ;i++ ) 
+		{
+			for (int space=0;space<=n-i-1 ;space++ ) 
+			{
+				System.out.print(" "+" ");
+			}
+			for (int star=0;star<=n ;star++ ) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}	
+	}
+}
