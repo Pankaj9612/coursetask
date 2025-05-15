2607fbc581dad9171983c6f354c07ea811114d27
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-05-15 16:51:20 +0530

    Add files via upload

---
+++ Cptn7.java
@@ -0,0 +1,22 @@
+class Cptn7
+{
+	public static void main(String[] args) 
+	{
+		int n=11;
+		for (int i=0;i<n ;i++ ) 
+		{
+			for (int j=0;j<n ;j++ ) 
+			{
+				if (j<i) 
+				{
+					System.out.print(" "+" ");		
+				}	
+				else
+				{
+					System.out.print("*"+" ");
+				}
+			}
+			System.out.println();
+		}	
+	}
+}
