ece9f7538a61cc0dfb31ec0ce26da69df61dad4b
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-05-29 18:20:04 +0530

    Add files via upload

---
+++ DefaultValues.java
@@ -0,0 +1,21 @@
+class DefaultValues
+{
+	static int a;
+	static byte b;
+	static short c;
+	static char d;
+	static long e;
+	static float f;
+	static double g;
+	static String h;
+	public static void main(String[] args) {
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+	}
+}	
