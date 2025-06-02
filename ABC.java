a8bfa95b1504561b006ec3e3e068effcf842fa72
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-02 23:00:57 +0530

    Add files via upload

---
+++ ABC.java
@@ -0,0 +1,12 @@
+public class ABC
+{
+	public static void main(String[] args) {
+		System.out.println("From Class ABC");
+	}
+}
+class  A
+{
+	public static void main(String[] args) {
+		System.out.println("From class A");
+	}
+}
