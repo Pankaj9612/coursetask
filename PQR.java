f3764388613a5b7d99b3b74c106fd1491ebc3e51
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-02 23:00:27 +0530

    Add files via upload

---
+++ PQR.java
@@ -0,0 +1,20 @@
+class A
+{
+	public static void main(String[] args) {
+		System.out.println("From class A");
+	}
+}
+
+class B
+{
+	public static void main(String[] args) {
+		System.out.println("From Class B");
+	}
+}
+
+class C
+{
+	public static void main(String[] args) {
+		System.out.println("From Class C");
+	}
+}
