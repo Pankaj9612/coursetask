b8edef4a1d84a70daf26fa75c6ee545e028db7c5
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-06 00:16:22 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,17 @@
+class Car
+{
+	String brand;
+	String model;
+	String tiers;
+	long price;
+	double milege;
+
+	public void  DisplayCar()
+	{
+		System.out.println("brand :"+brand);
+		System.out.println("model: "+model);
+		System.out.println("tiers: "+tiers);
+		System.out.println("price: "+price);
+		System.out.println("milege: "+milege);
+	}
+}
