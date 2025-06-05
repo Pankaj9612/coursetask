023bf5881ac6132e32267f4cb475623e1b801194
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-06 00:15:23 +0530

    Add files via upload

---
+++ Pen.java
@@ -0,0 +1,17 @@
+class Pen
+{
+	String type;
+	String color;
+	int price;
+	double width;
+	String ink;
+
+	public void DisplayPen()
+	{
+		System.out.println(type);
+		System.out.println(color);
+		System.out.println(price);
+		System.out.println(width);
+		System.out.println(ink);
+	}
+}
