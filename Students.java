fb731062927734177ca561236a518b138a6af2d0
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-05 23:03:19 +0530

    Add files via upload

---
+++ Students.java
@@ -0,0 +1,22 @@
+class Students
+{
+	String firstname;
+	String lastname;
+	long mobileno;
+	String emailid;
+	String degree;
+	double percentage;
+	int rollno;
+
+	public void displayStudents()
+	{
+		System.out.println("firstname: "+firstname);
+		System.out.println("lastname: "+lastname);
+		System.out.println("mobileno: "+mobileno);
+		System.out.println("emailid: "+emailid);
+		System.out.println("degree: "+degree);
+		System.out.println("percentage: "+percentage);
+		System.out.println("rollno: "+rollno);
+	}
+
+}
