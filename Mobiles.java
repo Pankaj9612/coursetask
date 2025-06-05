1e5b30a2ea5c4032efa474882de9e6452d9f01c1
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-05 23:03:45 +0530

    Add files via upload

---
+++ Mobiles.java
@@ -0,0 +1,22 @@
+class Mobiles
+{
+	String brand;
+	String name;
+	String color;
+	String seller;
+	int price;
+	int battery;
+	String processor;
+
+	public void displayMobile()
+	{
+		System.out.println("brand: "+brand);
+		System.out.println("name: "+name);
+		System.out.println("color: "+color);
+		System.out.println("seller: "+seller);
+		System.out.println("price: "+price);
+		System.out.println("battery: "+battery);
+		System.out.println("processor: "+processor);
+	}
+
+}
