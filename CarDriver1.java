2f6dffcddb0323edcdeddffa9db3cc5f0850f81e
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-06 22:42:59 +0530

    Add files via upload

---
+++ CarDriver1.java
@@ -0,0 +1,13 @@
+class CarDriver1
+{
+	public static void main(String[] args) {
+		Car1 m1= new Car1();
+	System.out.println(m1);	
+		m1.DisplayCar();
+		System.out.println("<--------------------->");
+		Car1 m2= new Car1();
+	System.out.println(m2);			
+		m2.DisplayCar1();
+		System.out.println("<-------------------->");
+	}
+}
