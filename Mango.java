011be20319e88dc46c4a98e7b3401dd3ec24c6d2
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-08 23:15:02 +0530

    Add files via upload

---
+++ Mango.java
@@ -0,0 +1,22 @@
+class Mango
+{
+	String  name;
+	String origin;
+	int price;
+
+
+	public void printName()
+	{
+		System.out.println("Name: "+name);
+	}
+
+	public void printPrice()
+	{
+		System.out.println("Price: "+price);
+	}
+
+	public void printOrigin()
+	{
+		System.out.println("Origin: "+origin);
+	}
+}
