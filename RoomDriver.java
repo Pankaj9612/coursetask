b4d9cddae57f09fdf211c319565ac2f24161e2ed
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-10 11:33:23 +0530

    Add files via upload

---
+++ RoomDriver.java
@@ -0,0 +1,12 @@
+public class RoomDriver 
+{
+    public static void main(String args[])
+    {
+        Room r1= new Room();
+        System.err.println(r1);
+
+        r1.displayRoomName();
+        r1.displayRoomArea();
+        r1.displayRoomDetails();
+    }   
+}
