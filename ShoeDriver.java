28e98b75c206c70d0950f1e5744d8dd8602c6ac9
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-11 11:31:00 +0530

    Add files via upload

---
+++ ShoeDriver.java
@@ -0,0 +1,12 @@
+class ShoeDriver
+{
+    public static void main(String args[])
+    {
+        Shoe s1=new Shoe();
+        s1.displayShoe();
+
+        Shoe s2= new Shoe("sparx",900,9,"stylish","black");
+
+        s2.displayShoe();
+    }
+}
