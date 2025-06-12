8dd96db6850d85fe6a66229c15eed31248fd831b
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-12 09:54:08 +0530

    Add files via upload

---
+++ ConChainDriver.java
@@ -0,0 +1,13 @@
+package application;
+
+public class ConChainDriver 
+{
+	public static void main(String args[])
+	{
+		ConChaining c1= new ConChaining();
+		c1.displayForm();
+		
+		ConChaining c2= new ConChaining("pankaj",22,965353637,"panku@gmail.com","dharashiv","AB+",353645,"pune");
+		c2.displayForm();
+	}
+}
