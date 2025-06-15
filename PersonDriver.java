4496e36cf71fc4b46d470db53e921bd515e7e5eb
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-15 12:25:44 +0530

    Add files via upload

---
+++ PersonDriver.java
@@ -0,0 +1,11 @@
+package application;
+
+public class PersonDriver {
+	public static void main(String[] args) {
+		Person p1= new Person("Pankaj",44,724646.58);
+		p1.setname("rohan");
+		p1.setage(32);
+		p1.setsal(1387465544.7);
+		p1.displayPerson();
+	}
+}
