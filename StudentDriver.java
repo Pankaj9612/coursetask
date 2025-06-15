b3b06a55ae6becdb2aa080622ff8b0ed3f3e72b2
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-15 12:24:45 +0530

    Add files via upload

---
+++ StudentDriver.java
@@ -0,0 +1,9 @@
+package application;
+
+public class StudentDriver {
+	public static void main(String[] args) {
+		Student s1= new Student("DBATU","KIT",347474858854l,"Rahul","Raosaheb",
+				"Shanti","dharahiv","ena@gmail.com",1234567890l,645755484.4848);
+		s1.displayinfo();
+	}
+}
