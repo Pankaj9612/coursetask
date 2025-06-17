b64fe111ab40c3da8cdf9e51541dad833afdd4df
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-17 11:14:45 +0530

    Add files via upload

--- PenDriver.java
+++ PenDriver.java
@@ -1,17 +1,9 @@
-class PenDriver
-{
-	public static void main(String[] args) 
-	{
-		Pen p1=new Pen();
-		System.out.println(p1);
+package application;
 
-		p1.type="Ball-Pen";
-		p1.color="Blue";
-		p1.price=5;
-		p1.width=1.2;
-		p1.ink="black";
-
-		p1.DisplayPen();
-		System.out.println("<--------------------------------->");	
+public class PenDriver {
+	public static void main(String[] args) {
+		Pen p1= new Pen("Cello","jel",25,2.1);
+		p1.displayPen();
+		p1.r1.displayrefeel();
 	}
 }
