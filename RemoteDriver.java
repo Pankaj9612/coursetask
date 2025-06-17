c19192b264bee4cfc0a09e13ac43d7275742bd8b
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-17 11:13:15 +0530

    Add files via upload

---
+++ RemoteDriver.java
@@ -0,0 +1,9 @@
+package application;
+
+public class RemoteDriver {
+		public static void main(String[] args) {
+			Remote r1=new Remote("ddh","Screentouch",1001);
+			r1.displayRemote();
+			r1.c1.displayCell();
+		}
+}
