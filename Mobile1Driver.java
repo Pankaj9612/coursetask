fec0239e0b926dd25e5fedeb4b9f652a86620b0a
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-17 11:11:38 +0530

    Add files via upload

---
+++ Mobile1Driver.java
@@ -0,0 +1,10 @@
+package application;
+
+public class Mobile1Driver {
+	public static void main(String[] args) {
+		Mobile1 m1= new Mobile1("samsung","galaxy m35","android",256,8,34748);
+		m1.displaymobile();
+		m1.b1.displayBattery();
+	}
+	
+}
