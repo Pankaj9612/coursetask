db4117158a82ed114e6577d60e405bab3d62c4fb
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-17 11:14:14 +0530

    Add files via upload

--- Pen.java
+++ Pen.java
@@ -1,17 +1,32 @@
-class Pen
-{
-	String type;
-	String color;
-	int price;
-	double width;
-	String ink;
+package application;
 
-	public void DisplayPen()
+public class Pen {
+	String name;
+	String type;
+	double price;
+	double size;
+	
+	refeel r1=new refeel("amo","jel",4.0);
+	
+	Pen()
 	{
+		
+	}
+	
+	Pen(String name,String type,double price,double size)
+	{
+		this.name=name;
+		this.type=type;
+		this.price=price;
+		this.size=size;
+		
+	}
+	
+	public void displayPen()
+	{
+		System.out.println(name);
 		System.out.println(type);
-		System.out.println(color);
 		System.out.println(price);
-		System.out.println(width);
-		System.out.println(ink);
+		System.out.println(size);
 	}
 }
