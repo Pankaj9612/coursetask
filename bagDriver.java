ea0b35bb4b0c3ecf7e1827a35d0692b9e4d628b7
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-18 10:38:28 +0530

    Add files via upload

---
+++ bagDriver.java
@@ -0,0 +1,10 @@
+package application;
+
+public class bagDriver {
+	public static void main(String[] args) {
+		Bag b1= new Bag("American","Sky bags",2500,8);
+		b1.displaybag();
+		b1.insertBook("Tech-max", "os", 150, 500, "Somesh");
+		b1.b1.displaybook();
+	}
+}
