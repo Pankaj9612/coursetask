8ee7d23e5b86e9a0163819d4da1e898131b51997
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-19 09:48:03 +0530

    Add files via upload

--- CarDriver.java
+++ CarDriver.java
@@ -1,40 +1,10 @@
-class CarDriver
-{
+package application;
+
+public class CarDriver {
 	public static void main(String[] args) {
-		Car m1= new Car();
-	System.out.println(m1);	
-
-		m1.brand="BMW";
-		m1.model="G20";
-		m1.tiers="Apollo";
-		m1.price=6000000;
-		m1.milege=12;
-
-		m1.DisplayCar();
-		System.out.println("<--------------------------------------->");
-
-		Car m2= new Car();
-	System.out.println(m2);	
-
-		m2.brand="Maruti Suzuki";
-		m2.model="Nano";
-		m2.tiers="MRF";
-		m2.price=300000;
-		m2.milege=28;
-
-		m2.DisplayCar();
-		System.out.println("<--------------------------------------->");
-
-		Car m3= new Car();
-	System.out.println(m3);	
-
-		m3.brand="Tata";
-		m3.model="Punch";
-		m3.tiers="solo";
-		m3.price=600000;
-		m3.milege=35;
-
-		m3.DisplayCar();
-		System.out.println("<--------------------------------------->");
+		
+		Car c1= new Car("Tata","Punch",100000,"petrol",24.0,"volvo","s1-volvo",800000);
+		c1.displayCar();
+		c1.e1.displayengine();
 	}
 }
---
+++ Employee.java
@@ -0,0 +1,19 @@
+package application;
+
+public class Employee {
+	String ename;
+	int eid;
+	
+	
+	public  void add()
+	{
+		System.out.println("35000");
+	}
+	
+	public static void main(String[] args) {
+		Employee e1= new Employee();
+		e1.ename="panakjraje";
+		e1.eid=4;
+		e1.add();
+	}
+}
