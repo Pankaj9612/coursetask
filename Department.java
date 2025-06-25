0d16a2a33e6cdcd2969473e4fd1baab54faca33a
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-06-25 09:49:15 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,49 @@
+package application;
+
+public class Department extends College{
+	
+	String type;
+	String hod;
+	int lec;
+	int noOfStudents;
+	int seats;
+	
+	public Department() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	Department(String uname, String vc, String uloc, int code, 
+			String utype,String collname, int collCode, int grade, 
+			int noOfDepts, String principal,String type,String hod,int lec,int noOfStudents,int seats)
+	{
+		this.uname = uname;
+		this.vc = vc;
+		this.uloc = uloc;
+		this.code = code;
+		this.utype = utype;
+		
+		this.collname = collname;
+		this.collCode = collCode;
+		this.grade = grade;
+		this.noOfDepts = noOfDepts;
+		this.principal = principal;
+		
+		this.type=type;
+		this.hod=hod;
+		this.lec=lec;
+		this.noOfStudents=noOfStudents;
+		this.seats=seats;
+	}
+	
+	public void displayDepartment()
+	{
+		displayUniversity();
+		displayCollege();
+		System.out.println(type);
+		System.out.println(hod);
+		System.out.println(lec);
+		System.out.println(noOfStudents);
+		System.out.println(seats);
+		System.out.println("-------------------------------");
+	}
+}
