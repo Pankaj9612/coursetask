4f35180dfda4a76cef5858aa2d69cd4ab6cc7c9e
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-01 22:34:08 +0530

    Add files via upload

---
+++ Kaju.java
@@ -0,0 +1,16 @@
+package application;
+
+public	final class Kaju {
+	
+String name="Pankaj";
+	
+	public  void display()
+	{
+		System.out.println(name);
+	}
+	
+	public static void main(String[] args) {
+		Kaju k1 =new Kaju();
+		k1.display();
+	}
+}
---
+++ Ramu.java
@@ -0,0 +1,18 @@
+package application;
+
+public final class Ramu extends A1{
+	
+String name="Pankaj";
+	
+	public  void display()
+	{
+		System.out.println(name);
+	}
+	
+	public static void main(String[] args) {
+		
+		Ramu r1=new Ramu();
+		
+		r1.display();
+	}
+}
