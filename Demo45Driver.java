a4050a276b265e035303be84ec0382a6ebbc6662
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-02 22:46:31 +0530

    Add files via upload

---
+++ Demo45Driver.java
@@ -0,0 +1,19 @@
+package application;
+
+public class Demo45Driver {
+	
+	public static void main(String[] args) {
+		
+		Demo5 d1= new Demo5();
+		
+		System.out.println(d1);
+		d1.drinking();
+		
+		Demo4 d2= new Demo5();
+		
+		System.out.println(d2);
+		
+		d2.drinking();
+		d1.Smoking();
+	}
+}
