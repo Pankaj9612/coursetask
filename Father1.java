061a9305b684b63fcdb6fbcfbdd5798a3ebdf3f2
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-03 22:39:58 +0530

    Add files via upload

---
+++ Father1.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Father1 {
+	
+	public void drinking()
+	{
+		System.out.println("Drinking");
+	}
+	
+	public void dancing()
+	{
+		System.out.println("Dancing");
+	}
+}
