85668027ae062abbe91c8b52a0d55973bc28b48c
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-03 22:42:11 +0530

    Add files via upload

---
+++ Studentsclass.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Studentsclass extends Teacher{
+	
+	public void Learning()
+	{
+		System.out.println("Learning is not easy");
+	}
+	
+	public void Studying()
+	{
+		System.out.println("Study well!");
+	}
+}
