f4704370f44df25583dc6fb710348a768c001f91
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-04 11:54:16 +0530

    Add files via upload

---
+++ Car3.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Car3 {
+
+	String brand="TATA";
+	String type="Normal Car";
+	double price=3000000;
+	
+	public void displayCar3()
+	{	
+		System.out.println(brand);
+		System.out.println(type);
+		System.out.println(price);
+	}
+}
---
+++ Car3Driver.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Car3Driver {
+
+	public static void main(String[] args) {
+		
+		Car3 c1= new Car3();
+		
+		c1.displayCar3();
+	}
+}
