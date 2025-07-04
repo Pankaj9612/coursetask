089d7c8a9311e4a80d9db68f3f9afbae32c412de
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-04 11:53:34 +0530

    Add files via upload

---
+++ Car2.java
@@ -0,0 +1,18 @@
+package application;
+
+public class Car2 {
+	
+	String brand;
+	String type;
+	double price;
+	
+	public void displayCar1()
+	{	
+		String brand="Honda";
+		System.out.println(brand);
+		System.out.println(this.brand);
+		System.out.println(this);
+		System.out.println(type);
+		System.out.println(price);
+	}
+}
---
+++ Car2Driver.java
@@ -0,0 +1,16 @@
+package application;
+
+public class Car2Driver {
+
+	public static void main(String[] args) {
+		
+		Car2 c1= new Car2();
+		
+		System.out.println(c1);
+		c1.brand="TATA";
+		c1.price=180000;
+		c1.type="SportCar";
+		c1.displayCar1();
+		
+	}
+}
