f82abb6c4dd59a1dc8b6a5b2e60ec5c224b9cd52
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-05 22:48:23 +0530

    Add files via upload

---
+++ D.java
@@ -0,0 +1,18 @@
+package application;
+
+public class D {
+
+	static int a=100;
+	
+	char ch='a';
+	
+	public static void test()
+	{
+		System.out.println("From test() of class D");
+	}
+	
+	public void demo()
+	{
+		System.out.println("from Demo() of class D");
+	}
+}
---
+++ DEDriver.java
@@ -0,0 +1,15 @@
+package application;
+
+public class DEDriver {
+
+	public static void main(String[] args) {
+		
+		D d1=new E();
+		
+		System.out.println(d1.ch);
+		System.out.println(d1.a);
+		
+		d1.demo();
+		d1.test();
+	}
+}
