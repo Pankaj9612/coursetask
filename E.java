4b86854f26f74f9759aeb95df73b4a1e6128d126
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-05 22:48:55 +0530

    Add files via upload

---
+++ E.java
@@ -0,0 +1,17 @@
+package application;
+
+public class E extends D {
+
+	static int a=10;
+	char ch='a';
+	
+	public void demo()
+	{
+		System.out.println("From demo() of class E");
+	}
+	
+	public static void test()
+	{
+		System.out.println("From test() of class E");
+	}
+}
