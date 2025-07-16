c26c5b052a723676c3287041c40e7cb8640e385d
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-16 22:50:24 +0530

    Add files via upload

---
+++ Mother.java
@@ -0,0 +1,12 @@
+package objectclass;
+
+public interface Mother {
+
+	double per=90;
+	void degree();
+	
+	public static void msg()
+	{
+		System.out.println("From mother interface");
+	}
+}
--- Son.java
+++ Son.java
@@ -1,9 +1,20 @@
-package application;
+package objectclass;
 
-public class Son extends Father1{
+public class Son implements Father,Mother{
+	public Son() {
+		// TODO Auto-generated constructor stub
+	}
 	
-	public void smoking()
+	public void degree()
 	{
-		System.out.println("Smoking");
+		System.out.println("BA");
+	}
+	
+	public static void main(String[] args) {
+		
+		Father f1=new Son();
+		f1.degree();
+		Father.msg1();
+		Mother.msg();	
 	}
 }
