4f3e514591c79db650ee83ac2f6414c151fe9c5d
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-22 19:22:17 +0530

    Add files via upload

---
+++ Exp8.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class Exp8 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("pankaj");
+			Thread.sleep(2000);
+		}
+		
+	}
+}
