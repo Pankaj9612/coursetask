793f7bad39b18105244d9f83abb5cf2c822d66d1
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-25 21:31:29 +0530

    Add files via upload

---
+++ Final1.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class Final1 {
+
+	public static void main(String[] args) {
+		
+		final int a;
+		a=100;
+		System.out.println(a);
+		//a=200  we cannot reassign and re-initailized it
+	}
+}
