fe3b54cb4c55740fe96adf170abcfb81c1a178a2
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-25 21:32:23 +0530

    Add files via upload

---
+++ Final2.java
@@ -0,0 +1,18 @@
+package pack1;
+
+public class Final2 {
+
+	static final int A=100;
+	static double PI;
+	
+	static
+	{
+		PI=22.0/7.0;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(A);
+		System.out.println(PI);
+	}
+}
