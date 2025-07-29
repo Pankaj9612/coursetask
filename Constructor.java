1bde988a14e9bf6e935b19252e183eaa1e36e53f
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-29 23:05:52 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,21 @@
+package application;
+
+public class Constructor {
+
+	String name;
+	int age;
+	String education;
+	
+	public Constructor() {
+	
+	}
+
+	public Constructor(String name, int age, String education) {
+	
+		this.name = name;
+		this.age = age;
+		this.education = education;
+	}
+	
+	
+}
---
+++ ConstructorDriver.java
@@ -0,0 +1,11 @@
+package application;
+
+public class ConstructorDriver {
+
+	public static void main(String[] args) {
+		
+		Constructor c1= new Constructor("Pankaj", 22, "Btech");
+		System.out.println(c1);
+		
+	}
+}
