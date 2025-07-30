5e338fce55ec6db0fe2ce4056bb56aab3e1948b0
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-30 19:50:59 +0530

    Add files via upload

---
+++ PrimitiveToString.java
@@ -0,0 +1,19 @@
+package application;
+
+public class PrimitiveToString {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		String ans=String.valueOf(a);
+		System.out.println(ans+20);
+		
+		boolean b=true;
+		String ans1=String.valueOf(b);
+		System.out.println(ans1+false);
+		
+		String ans2=String.valueOf('P');
+		System.out.println(ans2+"ankaj");
+				
+	}
+}
