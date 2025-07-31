cbf75fc7de4774c2b294735b3181e030bfcae4a4
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-07-31 20:19:22 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		
+		String [] names= {"Pankaj","Chinmay","Omkar","Rahul","Amit","Yogi","Abheejit","Abhay","Vaibhav"};
+		System.out.println(names[0]);
+		System.out.println(names[1]);
+		System.out.println(names[2]);
+		System.out.println(names[3]);
+		System.out.println(names[4]);
+		//System.out.println(names[9]);// ArrayOutOfBoundsException
+		//System.out.println(names[-19]);// ArrayOutOfBoundsException
+	}
+}
