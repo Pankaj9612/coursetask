c4de62a18f14649a6595fd1780e1c0f1a1b5559d
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-08-19 07:34:42 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,102 +1,40 @@
 package application;
 
-public class Student 
-{
-		private String universityName;
-		private String NameOfInstitute;
-		private long enrollmentno;
-		private String CandidateName;
-		private String FatherName;
-		private String motherName;
-		private String address;
-		private String emailId;
-		private long mobileno;
-		private double fess;
-		
-		
-		Student()
-		{
-		}
-		
-		Student(String universityName,String NameOfInstitute,long enrollmentno,String CandidateName,String FatherName,
-				String motherName,String address,String emailId,long mobileno,double fess)
-		{
-			this.universityName=universityName;
-			this.NameOfInstitute=NameOfInstitute;
-			this.enrollmentno=enrollmentno;
-			this.CandidateName=CandidateName;
-			this.FatherName=FatherName;
-			this.motherName=motherName;
-			this.address=address;
-			this.emailId=emailId;
-			this.mobileno=mobileno;
-			this.fess=fess;
-		}
-		
-		public String universityName()
-		{
-			return universityName;
-		}
-		
-		public String NameOfInstitute()
-		{
-			return NameOfInstitute;
-		}
-		
-		public long enrollmentno()
-		{
-			return  enrollmentno;
-		}
-		
-		public String CandidateName()
-		{
-			return CandidateName;
-		}
-		
-		public String FatherName()
-		{
-			return FatherName;
-		}
-		
-		public String motherName()
-		{
-			return motherName;
-		}
-		
-		public String address()
-		{
-			return address;
-		}
-		
-		public String emailId()
-		{
-			return emailId;
-		}
-		
-		public long mobileno()
-		{
-			return mobileno;
-			
-		}
-		
-		public double fess()
-		{
-			return fess;
-		}
-		
-		public void displayinfo()
-		{
-			System.out.println(universityName);
-			System.out.println(NameOfInstitute);
-			System.out.println(enrollmentno);
-			System.out.println(CandidateName);
-			System.out.println(FatherName);
-			System.out.println(motherName);
-			System.out.println(address);
-			System.out.println(emailId);
-			System.out.println(mobileno);
-			System.out.println(fess);
-			
-		}
-		
+public class Student {
+
+	String  name;
+	String degree;
+	int age;
+	int roll;
+	int marks;
+	
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Student(String name, String degree, int age, int roll, int marks) {
+		super();
+		this.name = name;
+		this.degree = degree;
+		this.age = age;
+		this.roll = roll;
+		this.marks = marks;
+	}
+	
+	@Override
+	public String toString() {
+		return "[name=" + name + ", degree=" + degree + ", age=" + age + ", roll=" + roll + ", marks=" + marks
+				+ "]";
+	}
+	
+	public void displayStudent()
+	{
+		System.out.println("Name:"+name);
+		System.out.println("Age:"+age);
+		System.out.println("Roll:"+roll);
+		System.out.println("Marks"+marks);
+		System.out.println("Degree:"+degree);
+	}
 }
--- StudentDriver.java
+++ StudentDriver.java
@@ -1,9 +1,32 @@
 package application;
-
 public class StudentDriver {
+
 	public static void main(String[] args) {
-		Student s1= new Student("DBATU","KIT",347474858854l,"Rahul","Raosaheb",
-				"Shanti","dharahiv","ena@gmail.com",1234567890l,645755484.4848);
-		s1.displayinfo();
+		
+		StudnetDb sddr1=new StudnetDb("Root","Root");
+		
+		sddr1.login("Root", "Root");
+		sddr1.addstudent("pankaj","Btech", 22, 123, 99);
+		sddr1.addstudent("Raj","Btech", 22, 124, 89);
+		sddr1.addstudent("kaju","Btech", 22, 143, 49);
+		sddr1.addstudent("kaji","Btech", 22, 153, 45);
+		
+		sddr1.searchbymarks(99);
+		sddr1.searchbyName("pankaj");
+		sddr1.searchbyroll(123);
+		sddr1.sortbyRoll();
+		sddr1.sortbyname();
+		sddr1.sortbyMarks();
+		sddr1.printDb();
+		sddr1.login("Root", "Root");
+		sddr1.removeByName("kaji");
+		sddr1.printDb();
+		sddr1.removebyroll(123);
+		sddr1.printDb();
+		sddr1.highestmarks();
+		sddr1.removebyspecificmarks(50);
+		
+		sddr1.updatetherecord("raj",155);
+		sddr1.printDb();
 	}
 }
